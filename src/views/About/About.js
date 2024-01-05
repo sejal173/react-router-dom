92ed3902d74a0ee5b1c957ddb2bbbe6797749032
sejal173 <sejalkakade123@gmail.com>
2024-01-05 11:15:36 +0530

    modify pages

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -5,7 +5,7 @@ export default function About(){
     return(
         <>
                 <Navbar/>
-                <h1>About</h1>
+                <h1>About Page</h1>
                 
         </>
     )
--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -5,7 +5,7 @@ export default function Contact(){
     return(
         <>
                     <Navbar/>
-                <h1>Contact</h1>
+                <h1>Contact page</h1>
                 
         </>
     )
