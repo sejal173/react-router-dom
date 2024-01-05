b7c4f4307324addee22940c7ef2bde1c507eab78
sejal173 <sejalkakade123@gmail.com>
2024-01-05 11:13:13 +0530

    changes in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,22 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-export default function Navbar(){
+export default function Navbar(){  
+
+    const currentpath = window.location.pathname;
+
+    function getActiveClass(path){
+        return currentpath === path ? "active" :"";
+    }
     return(
         <>
-                <div className="navbar">
-                        <span className="links">My Website</span>
-                        <Link to="/" className="links">Home</Link>
-                        <Link to="/Contact" className="links">Contact</Link>
-                        <Link to="/About" className="links">About</Link>
-                </div>
+                <div className="nav">
+                       <span className="brand links" >My Website</span>
+                        <Link to="/" className={getActiveClass('/')}>Home</Link>
+                        <Link to="/Contact" className={getActiveClass('/Contact')}>Contact</Link>
+                        <Link to="/About" className={getActiveClass('/About')}>About</Link>
+                        </div>
+                
         </>
     )
 }
